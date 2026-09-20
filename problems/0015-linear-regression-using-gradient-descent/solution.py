796aa8b498099576bd97f6a00bcc6e8b451d6d4c
gf-gonzalez <99415507+gf-gonzalez@users.noreply.github.com>
2026-09-20 01:38:14 +0000

    Update: Linear Regression Using Gradient Descent

--- problems/0015-linear-regression-using-gradient-descent/solution.py
+++ problems/0015-linear-regression-using-gradient-descent/solution.py
@@ -21,8 +21,12 @@ def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: floa
     for _ in range(iterations):
         #loss: (1/2m)*sum((X*theta - y)^2) = (1/2m)*( (X0*theta0 - y0)^2 + (X1*theta1 - y1)^2 + ... )
         #dloss: (1/m) * ((x0*theta0 - y0)*x0 + (x1*theta1 - y1)*x1 + ...)
-        #dloss: X.T * X * theta - X.T * y
-        #dloss
-        dloss = ((X.T).dot(X)).dot(theta) - (X.T).dot(y)
-        theta -= (alpha/m)*dloss
+        #dloss: (1/m) * (X.T * X * theta - X.T * y)
+        #dloss: (1/m) * X.T * (X* theta - y)
+        #dloss: (1/m) * X.T * (y_hat - y)
+        #dloss: (1/m) * X.T * (errors)
+        y_hat = X @ theta
+        errors = y_hat - y
+        dloss = (1/m) * (X.T @ errors) 
+        theta -= alpha*dloss
     return theta.flatten()
